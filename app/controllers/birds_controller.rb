afe4cefc3813ed279eec2889e96ea6eb7fea7a56
Charlie <peabody00@gmail.com>
2021-06-01 12:51:42 -0500

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,23 @@
 class BirdsController < ApplicationController
   def index
     @birds = Bird.all
-    render json: @birds
+    # render json: @birds
+    # render json: birds, only: [:id, :name, :species]
+    render json: birds, except: [:created_at, :updated_at]
+    # The above state is actually this:
+    # render json: birds.to_json(except: [:created_at, :updated_at])
+    # This is the magic rails provides in the background
+  end
+
+  def show
+    bird = Bird.find_by(id: params[:id])
+    # render json: bird
+    # render json: {id: bird.id, name: bird.name, species: bird.species }
+    # render json: bird.slice(:id, :name, :species)
+    if bird
+      render json: { id: bird.id, name: bird.name, species: bird.species }
+    else
+      render json: { message: 'Bird not found' }
+    end
   end
 end
